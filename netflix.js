93f07b2049d12890cc75bf78185c5b8b2bd23dcd
startCoding <abid302084@gmail.com>
2024-11-05 12:36:11 +0600

    add new featur

---
+++ netflix.js
@@ -0,0 +1,120 @@
+// Frequently Asked Questions 
+
+let dropdownBox = document.querySelector("#first_footer_box1");
+let dropdownBoxText = document.querySelector("#first_foot_box_text");
+let ic = document.querySelector("#box_icon1");
+let test = 0;
+
+dropdownBox.addEventListener("click", function(){
+   
+    if(test == 0){
+        dropdownBoxText.style.display = "block";
+        // ic.innerHTML = "×";
+        console.log("I'm working");
+        test = 1;
+    }else {
+        dropdownBoxText.style.display = "none";
+        test = 0;
+    }
+    
+});
+
+
+let dropdownBox2 = document.querySelector(".first_footer_box2");
+let dropdownBoxText2 = document.querySelector("#first_foot_box_text2");
+// let ic2 = document.querySelector("#box_icon1");
+let test2 = 0;
+
+dropdownBox2.addEventListener("click", function(){
+   
+    if(test2 == 0){
+        dropdownBoxText2.style.display = "block";
+        // ic.innerHTML = "×";
+        console.log("I'm working");
+        test2 = 1;
+    }else {
+        dropdownBoxText2.style.display = "none";
+        test2 = 0;
+    }
+    
+});
+
+
+
+let dropdownBox3 = document.querySelector(".first_footer_box3");
+let dropdownBoxText3 = document.querySelector("#first_foot_box_text3");
+// let ic2 = document.querySelector("#box_icon1");
+let test3 = 0;
+
+dropdownBox3.addEventListener("click", function(){
+   
+    if(test3 == 0){
+        dropdownBoxText3.style.display = "block";
+        // ic.innerHTML = "×";
+        console.log("I'm working");
+        test3 = 1;
+    }else {
+        dropdownBoxText3.style.display = "none";
+        test3 = 0;
+    }
+    
+});
+
+
+let dropdownBox4 = document.querySelector(".first_footer_box4");
+let dropdownBoxText4 = document.querySelector("#first_foot_box_text4");
+// let ic2 = document.querySelector("#box_icon1");
+let test4 = 0;
+
+dropdownBox4.addEventListener("click", function(){
+   
+    if(test4 == 0){
+        dropdownBoxText4.style.display = "block";
+        // ic.innerHTML = "×";
+        console.log("I'm working");
+        test4 = 1;
+    }else {
+        dropdownBoxText4.style.display = "none";
+        test4 = 0;
+    }
+    
+});
+
+let dropdownBox5 = document.querySelector(".first_footer_box5");
+let dropdownBoxText5 = document.querySelector("#first_foot_box_text5");
+// let ic2 = document.querySelector("#box_icon1");
+let test5 = 0;
+
+dropdownBox5.addEventListener("click", function(){
+   
+    if(test5 == 0){
+        dropdownBoxText5.style.display = "block";
+        // ic.innerHTML = "×";
+        console.log("I'm working");
+        test5 = 1;
+    }else {
+        dropdownBoxText5.style.display = "none";
+        test5 = 0;
+    }
+    
+});
+
+
+let dropdownBox6 = document.querySelector(".first_footer_box6");
+let dropdownBoxText6 = document.querySelector("#first_foot_box_text6");
+// let ic2 = document.querySelector("#box_icon1");
+let test6 = 0;
+
+dropdownBox6.addEventListener("click", function(){
+   
+    if(test6 == 0){
+        dropdownBoxText6.style.display = "block";
+        // ic.innerHTML = "×";
+        console.log("I'm working");
+        test6 = 1;
+    }else {
+        dropdownBoxText6.style.display = "none";
+        test6 = 0;
+    }
+    
+});
