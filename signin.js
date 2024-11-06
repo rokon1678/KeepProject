1990c9b9dd6f5cd8c506654e1d32bb5998e8aea9
startCoding <abid302084@gmail.com>
2024-11-06 12:07:17 +0600

    add new info

--- signin.js
+++ signin.js
@@ -6,18 +6,26 @@ let email = document.querySelector("#email").value;
 let password = document.querySelector(".password").value;
 
 if(email == "" || password == ""){
-    email.style.border = "1px solid red";
-    password.style.border = "1px solid red";
+    
     alert("please fill-up all");
+    
     console.log("I'm working");
     return false;
-}else if (password.length < 6 || password.length > 12 ){
+}else if(password.length < 6 || password.length > 12){
 
-    password.style.border = "1px solid red";
+   
     alert("please enter minumum 6 degits of password");
     return false;
 
-} else {
+} else if(isNaN(password)){
+
+   
+    alert("Only Numbers are allowed");
+    return false;
+
+} else{
+    let email = document.querySelector("#email").value;
+let password = document.querySelector(".password").value;
     return true;
 }
 
